47699b12c0d93a9482fd8fa4c2cff44e24d29bef
VincentLoveAndroid <674928145@qq.com>
2018-05-21 14:29:00 +0800

    Revert "Test"
    This reverts commit ddd10b38b0ab7ea2c00593d6a9b7b621b9909538.

--- app/src/main/java/com/example/mingren/customviewset/Sign.java
+++ app/src/main/java/com/example/mingren/customviewset/Sign.java
@@ -36,7 +36,6 @@ public class Sign {
             System.out.println("finally");
             System.out.println("标签");
             System.out.println("master");
-            System.out.println("test");
         }
     }
 }   
